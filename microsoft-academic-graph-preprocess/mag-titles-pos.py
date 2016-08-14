5581a526720fb31cea5f2efeccca7bc2d31e7205
Simon David HERNANDEZ <mail@sdavidhernandez.com>
2016-08-14 04:07:54 +0200

    Changing output options.

--- microsoft-academic-graph-preprocess/mag-titles-pos.py
+++ microsoft-academic-graph-preprocess/mag-titles-pos.py
@@ -6,23 +6,35 @@ from nltk.tag.perceptron import PerceptronTagger
 #from nltk.tokenize import WordPunctTokenizer as Tokenizer 
 from nltk.tokenize import TreebankWordTokenizer as Tokenizer
 
-def pos_titles_from(input_path):
-  line_number = 0
+def pos_titles_from(input_path, output_path = ""):
   finput = open(input_path, "r")
+  if output_path:
+    foutput = open(output_path, "w")
+  else:
+    foutput = sys.stdout
+
   tokenizer = Tokenizer()
   tagger = PerceptronTagger()
+
+  line_number = 0
   for line in finput:
     if line_number % 100000 == 0:
       print >> sys.stderr, "Status:", time.strftime("%d/%m/%Y %H:%M:%S"), "Count:", line_number
     line_number += 1
     try:
-      paper_id, title = line.split("\t")
-      tokens = tokenizer.tokenize(title.strip())
+      fields = line.strip().split("\t")
+      paper_id = fields[0]
+      title = fields[1]
+      print >> foutput, paper_id
+      tokens = tokenizer.tokenize(title)
       for token in tagger.tag(tokens):
-        print paper_id, token[0], token[1], token[0].lower()
-      print 
+        print >> foutput, token[0], token[1]
+      print >> foutput
     except:
       print >> sys.stderr, "Error:", line, sys.exc_info()
 
 if __name__ == "__main__":
-  pos_titles_from(sys.argv[1])
+  if sys.argv[2:]:
+    pos_titles_from(sys.argv[1], sys.argv[2])
+  else:
+    pos_titles_from(sys.argv[1])
