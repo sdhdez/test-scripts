7a364501f86ba4b3ca5ae78da5c4683314a6bccd
Simon David HERNANDEZ <mail@sdavidhernandez.com>
2016-09-04 05:25:20 +0200

    Script to extract terms from the given XML file of the acl-rd-tec-2.0 corpus.

--- acl-rd-tec-2.0-preprocess/terms_from_xml.py
+++ acl-rd-tec-2.0-preprocess/terms_from_xml.py
@@ -3,19 +3,18 @@ import sys
 import os.path
 from xml.dom import minidom
 
-element_to_extract = sys.argv[1]
-xml_file = sys.argv[2]
+try:
+    xml_file = sys.argv[1]
+except:
+    xml_file = sys.stdout
 
-text_extracted = {}
 if os.path.isfile(xml_file):
     try:
         xmldoc = minidom.parse(xml_file)
-        elements_list = xmldoc.getElementsByTagName(element_to_extract)
+        elements_list = xmldoc.getElementsByTagName("term")
         text_list = [e[0].nodeValue for e in [element.childNodes for element in elements_list] if e[0].nodeType == e[0].TEXT_NODE] 
         for t in text_list:
-            text_extracted.setdefault(t, 0)
-            text_extracted[t] += 1
+            print t
     except:
         print >> sys.stderr, "xml_file:", xml_file, sys.exc_info()[0]
 
-print text_extracted
