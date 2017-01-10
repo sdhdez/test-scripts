1a1dbd7a1a1dee2c65884d175d978ddf47c4ed56
Simon David HERNANDEZ <mail@sdavidhernandez.com>
2017-01-10 17:52:22 +0100

    Scripts to test SVM with the projections, training only with tokens in the projections.

---
+++ kpext/test_classification_with_projections_svm.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+TRAIN_DIR=$1;
+PROJECTIONS_DIR=$2;
+OUTPUT_DIR=$3;
+
+EVAL_SCRIPT=$4;
+EVAL_RESULTS_FILE="evaluation_results_tmp.dat";
+
+TEST_DIR=$5;
+
+DEBUG="";
+
+if [ ! -d $OUTPUT_DIR ]
+then
+    mkdir $OUTPUT_DIR;
+fi
+
+if [ -f $EVAL_RESULTS_FILE ]
+then 
+    echo $EVAL_RESULTS_FILE " already exists."
+    if [[ ! $DEBUG == "debug" ]]
+    then 
+        exit;
+    fi
+fi
+
+echo "#Id FilterMinCount precision recall f1score" >> $EVAL_RESULTS_FILE;
+
+tmp_last_count="";
+current_index=0;
+while read pos_seq_count CUR_PROJECTION
+do
+    if [[ $tmp_last_count != $pos_seq_count ]]
+    then
+        ((current_index++));
+        FULL_NEW_PATH=$OUTPUT_DIR"pos_seq_count_gt"$pos_seq_count;
+        if [ ! -d $FULL_NEW_PATH ]
+        then
+            mkdir $FULL_NEW_PATH;
+        fi
+
+        if [ -d $FULL_NEW_PATH ]
+        then
+            PROJECTION=$PROJECTIONS_DIR$CUR_PROJECTION
+            python kp_svm_with_projection.py $TRAIN_DIR $PROJECTION $FULL_NEW_PATH $DEBUG;
+            read foo precision recall f1score total < <(python $EVAL_SCRIPT $TEST_DIR $FULL_NEW_PATH types | grep "KEYPHRASE");
+            echo $current_index $pos_seq_count $precision $recall $f1score >> $EVAL_RESULTS_FILE;
+            read foo foo foo precision recall f1score total < <(python $EVAL_SCRIPT $TEST_DIR $FULL_NEW_PATH rel | grep "total");
+            echo $current_index $pos_seq_count $precision $recall $f1score >> $EVAL_RESULTS_FILE".2";
+        fi
+        if [[ $DEBUG == "debug" ]]
+        then 
+            break;
+        fi
+    fi
+    tmp_last_count=$pos_seq_count;
+done < <(ls -1 $PROJECTIONS_DIR | sed -e 's/\(pos_seq_count_gt\([0-9]\+\)\)/\2 \1/' | sort -n)
